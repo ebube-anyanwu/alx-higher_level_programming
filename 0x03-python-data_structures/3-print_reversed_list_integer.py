cd3847c9dab1f64d5cbee3a4976ed006ac10150e
ebube-anyanwu <ebubechianyanwu1@google.com>
2023-03-14 10:25:07 -0700

    last changes

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for ints in reversed(my_list):
-        print("{}".format(ints))
+    if len(my_list) > 0:
+        for ints in reversed(my_list):
+            print("{}".format(ints))
