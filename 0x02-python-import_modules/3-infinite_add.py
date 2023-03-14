ac888730755780661b7bbf9c77b8be4ad45d6c52
ebube-anyanwu <ebubechianyanwu1@google.com>
2023-03-13 17:39:47 -0700

    last changes

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    argsSum = 0
+    argsLen = len(argv) - 1
+
+    if argsLen >= 1:
+        for args in range(1, argsLen + 1):
+            argsSum += int(argv[args])
+        print(argsSum)
