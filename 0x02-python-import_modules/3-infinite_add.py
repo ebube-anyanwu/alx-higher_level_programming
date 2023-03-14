c843241adc1117250203720a39c5427e9fa9e7d9
ebube-anyanwu <ebubechianyanwu1@google.com>
2023-03-13 17:42:57 -0700

    last changes

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     if argsLen >= 1:
         for args in range(1, argsLen + 1):
             argsSum += int(argv[args])
-        print(argsSum)
+        print("{}".format(argsSum))
